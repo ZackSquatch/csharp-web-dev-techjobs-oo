b186d5b0dcdcbb20b8f41b83c94fcf34d8746509
Zack Sloan <ZackSloanWorks@gmail.com>
2021-01-23 12:45:55 -0600

    Forgot to save before last commit, added Equals test

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -11,6 +11,7 @@ namespace TechJobsTests
         {
             Job job1 = new Job();
             Job job2 = new Job();
+
             Assert.IsFalse(job1.Id == job2.Id);
         }
 
@@ -24,6 +25,15 @@ namespace TechJobsTests
             Assert.AreEqual(job.EmployerLocation.Value, "Desert");
             Assert.AreEqual(job.JobType.Value, "Quality Control");
             Assert.AreEqual(job.JobCoreCompetency, "Persistence");
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+
+            Assert.IsFalse(job1.Equals(job2));
         }
     }
 }
